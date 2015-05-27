36e76bc2020455d8cf7b255f1517ab4ec81b3066
sugar <sugar@imac.local>
2015-05-28 01:50:08 +0300

    Add tilt to video section

--- KHVideo/AddTiltViewController.swift
+++ KHVideo/AddTiltViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AddTiltViewController: CommonVideoViewController {
 
@@ -33,15 +34,30 @@ class AddTiltViewController: CommonVideoViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func applyVideoEffectsToComposition(composition: AVMutableVideoComposition!, size: CGSize) {
+        // 1 - Layer setup
+        var videoLayer = CALayer()
+        var parentLayer = CALayer()
+        
+        parentLayer.frame = CGRectMake(0, 0, size.width, size.height);
+        videoLayer.frame = CGRectMake(0, 0, size.width, size.height);
+        parentLayer.addSublayer(videoLayer)
+        
+        // 2 - Set up the transform
+        var identityTransform: CATransform3D = CATransform3DIdentity
+        
+        // 3 - Pick the direction
+        if tiltSegment.selectedSegmentIndex == 0 {
+            identityTransform.m34 = 1.0 / 1000 // greater the denominator lesser will be the transformation
+        } else if tiltSegment.selectedSegmentIndex == 1 {
+            identityTransform.m34 = 1.0 / -1000 // lesser the denominator lesser will be the transformation
+        }
+        // 4 - Rotate
+        videoLayer.transform = CATransform3DRotate(identityTransform, CGFloat(M_PI)/6.0, 1, 0, 0)
+        
+        // 5 - Composition
+        composition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, inLayer: parentLayer)
+        
     }
-    */
-
+    
 }
